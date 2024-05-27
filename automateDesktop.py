834bb2f130072c5b50ac7260deb67abcdbe967fa
Ben Currie <gameguy000@outlook.com>
2024-05-26 20:22:50 -0700

    created file to rename files

--- automateDesktop.py
+++ automateDesktop.py
@@ -1,8 +1,15 @@
 import os
-
+from pathlib import Path
 print(os.getcwd())
 
-os.chdir("/Users/gameg/OneDrive/Desktop")
+os.chdir("/Users/gameg/OneDrive/Desktop/test_python_folder")
 
 for file in os.listdir():
-    print(file)
+    f = Path(file)
+
+    name, ext = f.stem, f.suffix
+    file_name = name.replace(" ", "-")
+    file_name = file_name.split("-")
+    file_name = [s.strip() for s in file_name]
+    new_name = '_'.join(file_name)
+    os.rename(file, new_name)
